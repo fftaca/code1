6e55f1528b335516b9b2bceae5064f9beb5c0e1a
Facu <103712013+fftaca@users.noreply.github.com>
2024-03-25 21:34:06 -0300

    Prueba Cotización Dolar API.py

--- Prueba Cotización Dolar API.py
+++ Prueba Cotización Dolar API.py
@@ -1,53 +1,82 @@
+import tkinter as tk
+from tkinter import ttk
 import requests
 from bs4 import BeautifulSoup
-from termcolor import colored
 
-# URL de la página
-url = "https://www.infodolar.com/cotizacion-dolar-blue.aspx"
+# Func para obtener los datos de la pag y mostrarlos en la ventana
+def obtener_datos():
+    global contador  # Declaramos contador como global
+    contador += 1  # Incrementamos el contador
+    txt_contador.delete(0, tk.END)  # Borramos el contenido actual del Entry
+    txt_contador.insert(0, contador)  # Insertamos el nuevo valor del contador
+    
+    # codigo para obtener los datos de la pagina y mostrarlos
+    url = "https://www.infodolar.com/cotizacion-dolar-blue.aspx"
 
-# Realizar la solicitud GET a la página
-response = requests.get(url)
+    response = requests.get(url)
 
-# Comprobar si la solicitud fue exitosa (código de respuesta 200)
-if response.status_code == 200:
-    # Obtener el contenido HTML de la respuesta
-    html_content = response.text
-    
-    # Crear un objeto BeautifulSoup para procesar el contenido HTML
-    soup = BeautifulSoup(html_content, "html.parser")
-    
-    # Encontrar el elemento que contiene la fecha y hora
-    fecha_element = soup.find("abbr", class_="timeago date")
-    
-    # Extraer la fecha y hora
-    fecha = fecha_element["title"]
-    
-    # Encontrar los elementos que contienen los valores de compra y venta
-    valores_element = soup.find_all("td", class_="colCompraVenta")
-    
-    # Extraer los valores de compra y venta
-    compra = valores_element[0].text.strip()
-    venta = valores_element[1].text.strip()
-    
-    # Agregar espacio entre los renglones
-    print("\n")
-    
-    # Mostrar los datos centrados con más espacio
-    print(colored("Fecha y hora:", "yellow").center(40))
-    print(fecha.center(40))
-    
-    print("\n")
-    
-    print(colored("Valor de compra:", "green").center(40))
-    print(compra.center(40))
-    
-    print("\n")
-    
-    print(colored("Valor de venta:", "green").center(40))
-    print(venta.center(40))
-    
-    print("\n")
-    
-else:
-    # Mostrar un mensaje de error si la solicitud no fue exitosa
-    print("Error al obtener los datos de la página")
+    if response.status_code == 200:
+        html_content = response.text
+        soup = BeautifulSoup(html_content, "html.parser")
+
+        fecha_element = soup.find("abbr", class_="timeago date")
+        fecha = fecha_element["title"]
+
+        valores_element = soup.find_all("td", class_="colCompraVenta")
+        compra = valores_element[0].text.strip()
+        venta = valores_element[1].text.strip()
+
+        lbl_fecha.config(text=fecha)
+        lbl_compra.config(text=compra)
+        lbl_venta.config(text=venta)
+
+    else:
+        lbl_fecha.config(text="Error al obtener los datos")
+        lbl_compra.config(text="")
+        lbl_venta.config(text="")
+
+# Crear ventana de Tkinter
+root = tk.Tk()
+root.title("Cotización del Dólar Blue Actual")
+
+
+ancho_ventana = 345
+alto_ventana = 320
+root.geometry(f"{ancho_ventana}x{alto_ventana}")
+
+
+style = ttk.Style()
+style.configure("Boton.TButton", background="red", foreground="black")
+
+
+lbl_fecha = ttk.Label(root, text="", font=("Arial", 12, "bold"), justify="center")
+lbl_compra = ttk.Label(root, text="", font=("Arial", 12))
+lbl_venta = ttk.Label(root, text="", font=("Arial", 12))
+
+
+lbl_fecha.pack(pady=10)
+lbl_compra.pack(pady=10)
+lbl_venta.pack(pady=10)
+
+
+# Agreg un Entry para mostrar el contador
+contador = 0
+txt_contador = ttk.Entry(root, font=("Arial", 10, "bold"), width=1)
+txt_contador.pack(pady=5)
+txt_contador.insert(0, contador)  # Insertamos el valor inicial del contador
+
+# Config de estilo para el Entry
+style.configure("Custom.TEntry", background="lightgray", foreground="black", padding=5)
+
+# Aplica el estilo al Entry
+txt_contador.config(style="Custom.TEntry")
+
+btn_cerrar = ttk.Button(root, text="Cerrar", style="Boton.TButton", command=root.destroy)
+btn_cerrar.pack(side=tk.BOTTOM, padx=5, pady=10)
+
+btn_actualizar = ttk.Button(root, text="Actualizar", style="Boton.TButton", command=obtener_datos)
+btn_actualizar.pack(side=tk.BOTTOM, padx=5, pady=10)
+
+obtener_datos()
+
+root.mainloop()
